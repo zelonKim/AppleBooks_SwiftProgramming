28aa17c746fdfa9ff412984fd5383a46eb4ff72f
zelonKim <ksz18601@gmail.com>
2024-05-23 23:02:24 +0900

    Control Flow2

--- WelcomeToSwift/ControlFlow.swift
+++ WelcomeToSwift/ControlFlow.swift
@@ -30,10 +30,55 @@ for score in individualScores {
     if (score > 50) teamScore += 3 // 본문은 반드시 중괄호로 감싸줘야 함.
 }
 
-print(teamScore) // 9 (오류 발생) 
+print(teamScore) // (오류 발생) 
 */
 
 
 ///////////////////////////////////////
+
+
+/* 
+var optStr: String? = "Hello"
+print(optStr == nil) // false 
+*/
+
+/* 
+var optStr: String?
+print(optStr == nil) // true 
+*/
+
+/* 
+var optStr: String
+print(optStr == nil) // (오류 발생)
+*/
+
+
+/////////////////////////////
+
+
+/* 
+var optName: String = "John AppleSeed"
+var greeting = "Hello"
+
+if name = optName { greeting = "Hello, \(name)" }
+print(greeting) // 오류 발생 (cannot find 'name' in scope)
+*/
+
+
+/* 
+var optName: String = "John AppleSeed"
+var greeting = "Hello"
+
+if let name = optName { greeting = "Hello, \(name)" }
+print(greeting) // 오류 발생 (initializer for conditional binding must have Optional type)
+*/
+
+
+/* var optName: String? = "John AppleSeed"
+var greeting = "Hello"
+
+if let name = optName { greeting = "Hello, \(name)" }
+print(greeting) // 정상 실행  // Hello, John AppleSeed
+ */
 
 
