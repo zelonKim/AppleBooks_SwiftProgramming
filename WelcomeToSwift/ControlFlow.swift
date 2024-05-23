a5f55f069a3e4791c955b7b609d52107cd672c0d
zelonKim <ksz18601@gmail.com>
2024-05-23 22:45:55 +0900

    Control Flow1

---
+++ WelcomeToSwift/ControlFlow.swift
@@ -0,0 +1,39 @@
+/* 
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+
+for score in individualScores {
+    if score > 50 {teamScore += 3}
+}
+
+print(teamScore) // 9
+ */
+
+
+/* 
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+
+for score in individualScores {
+    if (score > 50) {teamScore += 3} // 조건식을 괄호로 감싸줘도 됨.
+}
+
+print(teamScore) // 9 (정상 실행)
+ */
+
+
+/* 
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+
+for score in individualScores {
+    if (score > 50) teamScore += 3 // 본문은 반드시 중괄호로 감싸줘야 함.
+}
+
+print(teamScore) // 9 (오류 발생) 
+*/
+
+
+///////////////////////////////////////
+
+
